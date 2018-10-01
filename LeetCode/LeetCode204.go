bbc5b0f763ad8eddfc6478b82566f869dc741aa7
wgx <13121155167@163.com>
2018-10-01 16:08:15 +0800

    leetcode202 2018/9/30

--- LeetCode/LeetCode204.go
+++ LeetCode/LeetCode204.go
@@ -23,6 +23,23 @@ func isPrime(x float64) bool {
 	return true
 }
 
+//func countPrimes(n int) int {
+//	notPrime := make([]bool, n)
+//	count := 0
+//
+//	for i := 2; i < n; i++ {
+//		if !notPrime[i] {
+//			count++
+//			if temp := i * i; temp < n {
+//				for j := temp; j < n; j += i {
+//					notPrime[j] = true
+//				}
+//			}
+//		}
+//	}
+//	return count
+//}
+
 func main()  {
 	fmt.Println(countPrimes(10))
 }
